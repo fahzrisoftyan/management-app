27e1e54fe2280e63d5da1c60767abb6242d2c88d
Greggi <acc.game.pertama@gmail.com>
2017-07-03 17:14:52 +0700

    ubah nama 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Management Project App
                 </div>
 
                 <div class="links">
